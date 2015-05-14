3937ea3ac899e48a72ab94b3a7f9b1be201d6cf7
unknown <mgoluboff@LTXNW788.int.lantronix.com>
2015-05-14 14:46:44 -0400

    Add functions to get Line configuration and set Line configuration.

--- xpwLib.js
+++ xpwLib.js
@@ -39,7 +39,7 @@ window.xpw = (function () {
 				postMsg +="&optionalItem=Transmitter&action=Hex Transmit ";		
 				// After the space we add the string we want to send (in hex)
 				postMsg += String(a2hex(args.message));	
-				xmlhttp=new XMLHttpRequest();
+				var xmlhttp=new XMLHttpRequest();
 				
 				if (typeof args.callback !== "undefined")
 					xmlhttp.onreadystatechange=function() {
@@ -63,7 +63,7 @@ window.xpw = (function () {
 			postMsg += line;
 			postMsg += "&optionalItem=Receiver&action=Hex Receive";
 	
-			xmlhttp=new XMLHttpRequest();
+			var xmlhttp=new XMLHttpRequest();
 	
 			if (typeof args.done !== "undefined") {
 				xmlhttp.onreadystatechange=function() {
@@ -94,6 +94,76 @@ window.xpw = (function () {
 			xmlhttp.open("POST", "/action/status", true);					// This is the URL for Status Actions
 			xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 			xmlhttp.send(postMsg);
+		},
+		getLineConfig: function(args) {
+			if (typeof args === "undefined")
+				return;
+			var line = (args.line == '1' || args.line == '2') ? args.line : '1';
+			var xmlhttp=new XMLHttpRequest();
+			
+			if (typeof args.done !== "undefined") {
+				xmlhttp.onreadystatechange=function() {
+					if (xmlhttp.readyState==4) {
+						if (xmlhttp.status==200) {
+							var config = {};
+							var xmlDoc=xmlhttp.responseXML;
+							var items = xmlDoc.getElementsByTagName("configitem");
+				
+							for(var i=0;i<items.length;i++){
+								if(items[i].getElementsByTagName("value")[0].childNodes.length > 0) {
+									config[items[i].getAttribute('name')] = items[i].getElementsByTagName("value")[0].childNodes[0].nodeValue;
+								}
+							} 
+							
+							args.done({success:true, lineConfig:config});
+						} else {
+							args.done({success:false});
+						}
+					}
+				}
+			}
+			
+			var postMsg = "optionalGroupList=Line:"+line;
+			
+			xmlhttp.open("POST", "/export/config", true);					// This is the URL for Status Actions
+			xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+			xmlhttp.send(postMsg);
+		},
+		setLineConfig: function(args) {
+			if (typeof args === "undefined" || typeof args.line === "undefined" || typeof args.items === "undefined")
+				return;
+			var line = (args.line == '1' || args.line == '2') ? args.line : '1';
+			var xmlhttp=new XMLHttpRequest();
+			
+			var postMsg = "<?xml version=\"1.0\" standalone=\"yes\"?><!-- Automatically generated XML --><!DOCTYPE configrecord [  <!ELEMENT configrecord (configgroup+)><!ELEMENT configgroup (configitem+)><!ELEMENT configitem (value+)><!ELEMENT value (#PCDATA)><!ATTLIST configrecord version CDATA #IMPLIED><!ATTLIST configgroup name CDATA #IMPLIED><!ATTLIST configgroup instance CDATA #IMPLIED><!ATTLIST configitem name CDATA #IMPLIED><!ATTLIST configitem instance CDATA #IMPLIED><!ATTLIST value name CDATA #IMPLIED>]><configrecord version = \"0.1.0.1\"><configgroup name = \"Line\" instance = \""+line+"\">";
+			
+			for(var name in args.items) {
+				postMsg += "<configitem name = \""+name+"\"><value>"+args.items[name]+"</value></configitem>";
+			}
+			postMsg+="</configgroup></configrecord>"
+			
+			var fd = new FormData();
+			fd.append("configrecord", postMsg);
+			
+			if (typeof args.done !== "undefined") {
+				xmlhttp.onreadystatechange=function() {
+					if (xmlhttp.readyState==4) {
+						if (xmlhttp.status==200) {
+							var config = {};
+							var xmlDoc=xmlhttp.responseXML;
+							var result = xmlDoc.getElementsByTagName("result");
+				
+							if(result[0].childNodes[0].nodeValue=="Succeeded") {
+								args.done({success:true});
+							} else {
+								args.done({success:false});
+							}
+						}
+					}
+				}
+			}
+			xmlhttp.open("POST", "/import/config", true);					// This is the URL for Status Actions
+			xmlhttp.send(fd);
 		}
 	};
 	
